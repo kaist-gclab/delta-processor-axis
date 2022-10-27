59711a98b4cf8a834000c16233016421c89c90e0
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-10-28 07:56:42 +0900

    수행 시간 직접 측정

--- run-powercrust-test.sh
+++ run-powercrust-test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-time (
-    sampler/sampler <input-3d-model.pts >input-3d-model-preprocessed.pts
-    powercrust/powercrust -m 10000 -i input-3d-model-preprocessed.pts
-)
+SECONDS=0
+
+sampler/sampler <input-3d-model.pts >input-3d-model-preprocessed.pts
+powercrust/powercrust -s 20221028 -m 10000 -i input-3d-model-preprocessed.pts
+
+END=$SECONDS
+echo "Elapsed time: $END seconds."
